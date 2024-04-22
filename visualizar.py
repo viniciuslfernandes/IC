66d438a74c5172124fde0cb585a2e77f7296797c
Gabriel DESKTOP <leirbagrp@gmail.com>
2024-04-22 09:55:26 -0300

    wip

--- visualizar.py
+++ visualizar.py
@@ -3,11 +3,39 @@ from matplotlib import pyplot as plt
 from matplotlib import cm as cm
 import networkx as nx
 
-def visualize_network():
-    #arquivo = './gabriel/conexoes.txt'
-    arquivo = './vinicius/conexoes_vinicius.txt'
+def get_list_list(particionamento):
+    lista_de_listas = []
+    comunidades = set(particionamento.values())
+
+    for comunidade in comunidades:
+        indices_comunidade = [indice for indice, classe in particionamento.items() if classe == comunidade]
+        lista_de_listas.append(indices_comunidade)
     
-    # Constrói o grafo
+    return lista_de_listas
+
+def printComunidades(l1, arq):
+    pessoasArquivo = {}
+    with open(arq, "r") as f:
+        next(f)
+        for line in f:
+            fields = line.strip().split('\t')
+            if len(fields) >= 4:
+                nome, apelido, cidade = fields[1], fields[2], fields[3]
+                pessoasArquivo[fields[0]] = f"{fields[0]} - {nome}, {apelido}, {cidade}"
+
+    i = 1
+    # with open('./gabriel/comunidades.txt', "w") as f:
+    with open('./vinicius/comunidades_vinicius.txt', "w") as f:
+        for pessoas in l1:
+            f.write(f"Comunidade {i}: \n")
+            for pessoa in pessoas:
+                f.write(pessoasArquivo[str(pessoa)] + "\n")
+            i += 1
+
+def visualize_network():
+    # arquivo = './gabriel/conexoes.txt'
+    arquivo = './vinicius/conexoes_vinicius.txt'
+
     G = nx.Graph()
     
     with open(arquivo, 'r') as f:
@@ -18,26 +46,20 @@ def visualize_network():
             num3 = int(novaLinha.split(' ')[2])
             G.add_weighted_edges_from([(num1, num2, num3)])
 
-    # Aplica o algoritmo de detecção de comunidades Louvain
-    partition = community_louvain.best_partition(G)
-
-    # Posiciona os nós usando o algoritmo de layout spring
     pos = nx.spring_layout(G)
-
-    # Define o mapa de cores com base no número de comunidades
-    cmap = plt.get_cmap('viridis', max(partition.values()) + 1)
-
-    # Desenha os nós com cores diferentes com base em sua pertinência à comunidade
-    nx.draw_networkx_nodes(G, pos, partition.keys(), cmap=cmap, node_color=list(partition.values()))
-
-    # Desenha as arestas
-    nx.draw_networkx_edges(G, pos, alpha=0.5)
-
-    # Desenha as etiquetas dos nós
-    nx.draw_networkx_labels(G, pos, font_size=8, font_color='black')
-
-    # Exibe a visualização da rede
+    partitions = community_louvain.best_partition(G)
+    comunidades = set(partitions.values())
+    num_comunidades = len(comunidades)
+    print('Número de comunidades:', num_comunidades)
+    cores = ['blue', 'green', 'yellow', 'purple', 'orange']
+    nx.draw_networkx_nodes(G, pos, node_size=50, node_color=[cores[partitions[node]] for node in G.nodes()], alpha=0.5)
+    nx.draw_networkx_edges(G, pos, alpha=0.1)
+    nx.draw_networkx_labels(G, pos, font_size=6, font_color='black')
     plt.show()
+    
+    comunidade_ego = get_list_list(partitions)
+    # printComunidades(comunidade_ego, './gabriel/pessoas.txt')
+    printComunidades(comunidade_ego, './vinicius/pessoas_vinicius.txt')
 
 if __name__ == '__main__':
     visualize_network()
